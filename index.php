9779fa925a9cb71d86c197a2d9dcc3f58bad0371
Deimos <32775420+deimos75@users.noreply.github.com>
2017-10-17 17:26:10 +0200

    Ajout index
    Ajout de la page d'accueil du TP "blog".

--- index.php
+++ index.php
@@ -1,58 +1,49 @@
-<!-- index -->
-<!DOCTYPE html>
+<!-- Index -->
+
+
 <html>
     <head>
-        <meta charset="utf-8" />
         <title>Blog - Accueil</title>
-        <link href="blog.css" rel="stylesheet"/>
+        <link rel="stylesheet" href="blog.css" />
+        <meta charset="utf-8" />
     </head>
+
     <body>
+        <!-- Connexion à la BDD "blog" -->
         <?php
-        // Connexion à MySQL
-        try{
-            // Objet de la requête
-            $bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 
-            'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-        }catch(Exception $e){
-            die('Erreur : ' . $e->getMessage());
+        try
+        {
+            $bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
         }
+        catch (Exception $e)
+        {
+                die('Erreur : ' . $e->getMessage());
+        }
+
+
+        // On récupère tout le contenu de la table "billets"
+        $reponse = $bdd->query('SELECT * FROM billets ORDER BY titre DESC');
+        //$reponseContenu = $bdd->query('SELECT contenu FROM billets ORDER BY contenu DESC');
         ?>
         
-        
-        <h1>Mon super blog !</h1>
-        <p>Derniers billets du blog</p>
-        
-        <div class="news">
-            <h3>Titre</h3>
-            <p> 
-                Paragraphe<br>
-                <i><a href="commentaires.php">Commentaire</a></i>
-            </p>
-        </div>
-        
+        <h1>Mon super blog</h1>
+        <p>Derniers billets du blog:</p>
+
+        <?php
+        // On affiche chaque entrée une à une
+        while ($donnees = $reponse->fetch()){
+            ?>
+                <div class="news">
+                    <h3><?php echo $donnees['titre']; 
+                        echo '<i> le '.$donnees['date_creation'].'</i>';?><br/></h3>
+                    <p><?php echo $donnees['contenu'] ?> <br/>
+                    <i><a href="commentaires.php">Commentaires</a></i>
+                    </p>
+                </div>
+            <?php
+        }
+
+        $reponse->closeCursor(); // Termine le traitement de la requête
+        ?>
     </body>
 </html>
-
-
-
-<?php
-        // Connexion à MySQL
-        try{
-            // Objet de la requête
-            $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 
-            'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-        }catch(Exception $e){
-            die('Erreur : ' . $e->getMessage());
-        }
-        
-        // On récupère le contenu de la table jeux_video
-        $reponse = $bdd->query('SELECT * FROM jeux_video');
-        // Affiche les noms
-        $reponse = $bdd->query('SELECT nom FROM jeux_video');
-        while ($donnees = $reponse->fetch()){
-            echo $donnees['nom'] . '<br />';
-        }
-        
-        $reponse->closeCursor(); // Termine le traitement de la requête
-?>        
-        
