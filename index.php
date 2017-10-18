0c27be855793d702780b66e46388f228f6d5c4cf
Deimos <32775420+deimos75@users.noreply.github.com>
2017-10-18 18:07:24 +0200

    Amélioration TP blog: Pagination en cours

--- index.php
+++ index.php
@@ -12,16 +12,22 @@
         //Connexion à la BDD "blog"
         include("connexionBDD.php");
         
+        // On compte le nombre de billets
+        $requeteNbBillet = $bdd->query('SELECT COUNT(*) AS nb_billets FROM billets');
+        $nbBillets = $requeteNbBillet->fetch();
+        $N_billets = $nbBillets[0];
+        
+        // Nombre de page
+        $N_pages = ceil($N_billets / 5);
+        
         // On récupère tout le contenu de la table "billets"
         $requete = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS dateFR_creation FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
-        
-        //$requete = $bdd->query('SELECT id, titre, contenu, date_creation FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
-        
         ?>
         
         <h1>Mon super blog</h1>
         <p>Derniers billets du blog:</p>
-
+        <p>Il y a au total <?php echo $N_billets; ?> billets et <?php echo $N_pages ?> pages.</p>
+        
         <?php
         // On affiche chaque entrée une à une
         while ($donnees = $requete->fetch()){
